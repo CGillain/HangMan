25729acfcba5e9710bc024667e1d3b35013e2ba8
CGillain <59823370+CGillain@users.noreply.github.com>
2020-03-22 15:10:09 +0100

    Starting coding the game

--- HangMan_Game.py
+++ HangMan_Game.py
@@ -15,7 +15,6 @@ window = Tk()
 
 class hangman:
 
-
     def __init__(self, window):
         self.window = window
         self.window.resizable(width=True, height=True)
@@ -31,46 +30,33 @@ class hangman:
         self.photo = ImageTk.PhotoImage(self.photo)
         self.label= Label(window, image= self.photo).grid(row=2, column=2)
 
+
         # Word to guess
         self.guess_word = random.choice(Word_List.word_list)
         self.guess_word= str(self.guess_word)
         print(self.guess_word)
 
+        # Secret word showing with _
         self.letters_word = "_ " * len(self.guess_word)
-        #print(type(self.letters_word))
         self.word= Label(window, text="Guess the word: " + self.letters_word, font=("Calibri", 20), fg="black").grid(row=3, column=2)
+
 
         # Start Guessing
         self.letter_label= Label(window, text="Enter a letter: ", font=("Calibri", 20), fg="black").grid(row=3, column=3)
-
         self.letter_input = Entry(window)
         self.letter_input.grid(row=4, column=3)
-        # self.letter_input = str(self.letter_input)
         self.letter_input = self.letter_input.get()
-        #print(type(self.letter_input))
-
 
         # Check if letter in word
-
         self.letter_input = Entry(window)
         self.letter_input.grid(row=4, column=3)
-        # self.letter_input = str(self.letter_input)
-        self.letter_input = self.letter_input.get()
-
-        if self.letter_input in self.guess_word:
-            self.alphabet = "abcdefghijklmnopqrstuvwxyz"
-            self.alphabet.replace(self.letter_input, "")
-            self.letters_word_1= self.letters_word.replace(self.alphabet, "_ ")
-            self.word_1 = Label(window, text="Guess the word: " + self.letters_word_1, font=("Calibri", 20),
-                              fg="black").grid(row=3, column=2)
-
-        else:
-            self.photo_2= Image.open("2.png")
-            self.photo_2 = self.photo_2.resize((500, 500), Image.ANTIALIAS)
-            self.photo_2 = ImageTk.PhotoImage(self.photo_2)
-            self.label_2= Label(window, image= self.photo_2).grid(row=2, column=2)
+        self.letter_input = self.letter_input.get()    # getting the input of user
 
 
+
+
+
+window.mainloop()
 
 hangman = hangman(window)
 
--- test1.py
+++ test1.py
@@ -1,134 +1,60 @@
-import random
-from tkinter import *  # Import tkinter
+from tkinter import Tk, Label, Button, Entry, IntVar, END, W, E
 
+class Calculator:
 
-# Find if pressed letter is in word or not, update screen
-def keyPressed(event):
-    # modifying below global variable locally
-    global count, latestStr, missedLetter
+    def __init__(self, master):
+        self.master = master
+        master.title("Calculator")
 
-    current = event.char  # get the pressed key
+        self.total = 0
+        self.entered_number = 0
 
-    # Check for non-letters
-    if not current.isalpha():
-        print("\t", current, " is not a letter.")
-        return
+        self.total_label_text = IntVar()
+        self.total_label_text.set(self.total)
+        self.total_label = Label(master, textvariable=self.total_label_text)
 
-    # Check if current letter is already guessed and missed
-    if current in missedLetters:
-        print("\t", current, " is already in missed list")
-        return  # Do nothing
+        self.label = Label(master, text="Total:")
 
-    # Check if current alphabet is in original word
-    if current in word:
-        # Remove spaces if any in latestStr string
-        # Convert it into a list
-        latestStr_list = list(latestStr.replace("", ""))
-        for i in range(0, len(word)):  # Traverse list
-            # Check if current letter exist in word
+        vcmd = master.register(self.validate) # we have to wrap the command
+        self.entry = Entry(master, validate="key", validatecommand=(vcmd, '%P'))
 
-            if word[i] == current:
-                # Add the current alphabet in list
-                latestStr_list[i] = word[i]
-        latestStr = ''.join(latestStr_list)  # Append space
+        self.add_button = Button(master, text="+", command=lambda: self.update("add"))
+        self.subtract_button = Button(master, text="-", command=lambda: self.update("subtract"))
+        self.reset_button = Button(master, text="Reset", command=lambda: self.update("reset"))
 
-        # Prepare message to display on screen
-        msg1 = "Guess a word: " + latestStr
-        msg2 = "Missed letters: " + missedLetters
+        # LAYOUT
 
-    else:  # Current alphabet is not in original word
-        missedLetter += (current + '')  # Add letter to missed
-        count += 1  # Increment number of attempts
+        self.label.grid(row=0, column=0, sticky=W)
+        self.total_label.grid(row=0, column=1, columnspan=2, sticky=E)
 
-        if count < 7:  # Attemtpt finished
-            # Prepare message to display on screen
-            msg1 = "Guess a word: " + latestStr
-            msg2 = "Missed letter: " + missedLetters
-        else:  # Finished all attempts
-            # Display failure message, check for continue?
-            msg1 = "Sorry! The word is: " + word
-            msg2 = "To continue the game, press ENTER"
+        self.entry.grid(row=1, column=0, columnspan=3, sticky=W+E)
 
-    if '*' not in latestStr:  # Finished with success
-        # Display success message, check for 1 more round
-        msg1 = "Congrts! The word is: " + word
-        msg2 = "To continue the game, press ENTER"
+        self.add_button.grid(row=2, column=0)
+        self.subtract_button.grid(row=2, column=1)
+        self.reset_button.grid(row=2, column=2, sticky=W+E)
 
-    # Draw the latest hangman, display message and return
-    draw(count, msg1, msg2)
-    return
+    def validate(self, new_text):
+        if not new_text: # the field is being cleared
+            self.entered_number = 0
+            return True
 
+        try:
+            self.entered_number = int(new_text)
+            return True
+        except ValueError:
+            return False
 
-# Draw the latest hangman
-def draw(count, msg1, msg2):
-    canvas.delete("hang")  # Delete previous picture, if any
-    size = 400  # Screen size
+    def update(self, method):
+        if method == "add":
+            self.total += self.entered_number
+        elif method == "subtract":
+            self.total -= self.entered_number
+        else: # reset
+            self.total = 0
 
-    # Draw base and hook
-    canvas.create_line(10, size - 10, 50, size - 10)
-    canvas.create_line(30, size - 10, 30, 50)
-    canvas.create_line(30, 50, 200, 50)
-    radius = 30
-    if count >= 1:  # Draw sting to neck
-        canvas.create_line(200, 50, 200, 70, tags="hang")
-    if count >= 2:  # Draw face
-        canvas.create_line(200 - radius, 100 - radius,
-                           200 + radius, 100 + radius, tags="hang")
-    if count >= 3:  # Draw first arm
-        canvas.create_line(170, 100, 100, 150, tags="hang")
-    if count >= 4:  # Draw second arm
-        canvas.create_line(230, 100, 300, 150, tags="hang")
-    if count >= 5:  # Draw body length
-        canvas.create_line(200, 130, 200, 250, tags="hang")
-    if count >= 6:  # Draw one leg
-        canvas.create_line(200, 250, 100, 300, tags="hang")
-    if count >= 7:  # Draw second leg
-        canvas.create_line(200, 250, 300, 300, tags="hang")
+        self.total_label_text.set(self.total)
+        self.entry.delete(0, END)
 
-    # Display message
-    canvas.create_text(200, size - 30, text=msg1, tags="hang")
-    canvas.create_text(200, size - 10, text=msg2, tags="hang")
-
-
-# Start a new game
-def newGame():
-    # Modifying below global variable locally
-    global count, latestStr, word, missedLetters
-    # Randomly select a word from list
-    word = random.choice(words)
-    latestStr = "* " * len(word)
-    count = 0  # Counter for number of attempts
-    missedLetters = ''  # List of missed alphabets
-    msg1 = "Guess a word: " + latestStr
-    msg2 = ""
-    # Draw the latest hangman, display messages
-    draw(count, msg1, msg2)
-
-
-def play(event):
-    newGame()
-
-
-# Create a window, set its title
-window = Tk()
-window.title("Hangman")
-
-# Global variables
-word = latestStr = missedLetters = ''
-count = 0
-
-# create a canvas, add it to window
-size = 400
-canvas = Canvas(window, width=size, height=size)
-canvas.pack()  # Hold the pictue
-
-# List of words
-words = ["write", "that", "Program", "hello", "welcome"]
-newGame()  # play a new game
-
-# Keep playing till users presses enter key
-canvas.focus_set()  # Required as usual focus is on mouse
-canvas.bind(",play")
-canvas.bind(",keyPressed")  # use presses any key
-
-window.mainloop()  # Create an event loop
+root = Tk()
+my_gui = Calculator(root)
+root.mainloop()
